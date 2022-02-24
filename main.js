e753af6c6b1ee2e1d6d66edb21f519d656f4394d
Ferdianto <ferdiantojp24@gmail.com>
2022-02-23 20:14:14 -0500

    local storage + sorting buttons

--- main.js
+++ main.js
@@ -7,7 +7,15 @@ class Task {
   }
 }
 
-// const arrayOfTasks = []; // can be useful for sorting feature
+let taskList;
+
+if (localStorage.getItem("tasksSorted")) {
+  taskList = JSON.parse(localStorage.getItem("tasksSorted"));
+} else if (localStorage.getItem("tasks")) {
+  taskList = JSON.parse(localStorage.getItem("tasks"));
+} else {
+  taskList = [];
+}
 
 // DOM Selections
 const myForm = document.querySelector("#my-form");
@@ -15,10 +23,42 @@ const titleInput = document.querySelector("#tasktitle");
 const dateInput = document.querySelector("#deadline");
 const notesInput = document.querySelector("#notes");
 const msg = document.querySelector(".msg");
-const taskList = document.querySelector("#tasklist");
+const ulTasks = document.querySelector("#tasklist");
+const sortDateBtn = document.querySelector("#sort-date-btn");
+const sortCompBtn = document.querySelector("#sort-comp-btn");
+const resetBtn = document.querySelector("#reset-btn");
+const sortDateVal = localStorage.getItem("sortDate");
+const sortCompVal = localStorage.getItem("sortComp");
+
+// Events
+sortDateBtn.addEventListener("click", (e) => {
+  if (sortDateVal === "true") {
+    localStorage.setItem("sortDate", "false");
+    sortDesDeadline();
+  } else {
+    localStorage.setItem("sortDate", "true");
+    sortAscDeadline();
+  }
+});
+
+sortCompBtn.addEventListener("click", (e) => {
+  if (sortCompVal === "true") {
+    localStorage.setItem("sortComp", "false");
+    sortComplete();
+  } else {
+    localStorage.setItem("sortComp", "true");
+    sortIncomplete();
+  }
+});
+
+// resetBtn.addEventListener("click", (e) => {
+//   localStorage.removeItem("tasksSorted");
+//   localStorage.removeItem("sortDate");
+//   localStorage.removeItem("sortComp");
+//   location.reload();
+// });
 
 myForm.addEventListener("submit", onSubmit);
-
 function onSubmit(e) {
   e.preventDefault();
 
@@ -30,57 +70,102 @@ function onSubmit(e) {
       msg.classList.remove("error");
     }, 3000);
   } else {
-    const newTask = new Task(
-      titleInput.value,
-      dateInput.value,
-      notesInput.value
+    taskList.push(
+      new Task(titleInput.value, dateInput.value, notesInput.value)
     );
+    console.log(taskList);
+    titleInput.value = "";
+    dateInput.value = "";
+    notesInput.value = "";
+    localStorage.setItem("tasks", JSON.stringify(taskList));
+    location.reload();
+  }
+}
 
-    // arrayOfTasks.push(newTask); // can be useful for sorting feature
+// displaying stored taskList
+const liOutput = (description, deadline, notes, isCompleted) => {
+  const li = document.createElement("li");
 
-    const descriptionSpan = document.createElement("span");
-    descriptionSpan.setAttribute("class", "task-title");
-    descriptionSpan.appendChild(document.createTextNode(newTask.title));
+  const descSpan = document.createElement("span");
+  descSpan.setAttribute("class", "task-title");
+  descSpan.appendChild(document.createTextNode(description));
+  li.appendChild(descSpan);
 
-    const li = document.createElement("li");
-    li.appendChild(descriptionSpan);
-    if (newTask.deadline != "") {
-      li.appendChild(document.createElement("br"));
-      li.appendChild(
-        document.createTextNode(
-          `Deadline: ${new Date(newTask.deadline)
-            .toUTCString()
-            .substring(0, 16)}`
-        )
-      );
-    }
-    if (newTask.notes != "") {
-      li.appendChild(document.createElement("br"));
-      li.appendChild(document.createTextNode(`Notes: ${newTask.notes}`));
-    }
+  if (deadline !== "") {
+    li.appendChild(document.createElement("br"));
+    li.appendChild(
+      document.createTextNode(
+        `Deadline: ${new Date(deadline).toUTCString().substring(0, 16)}`
+      )
+    );
+  }
+  if (notes !== "") {
+    li.appendChild(document.createElement("br"));
+    li.appendChild(document.createTextNode(`Notes: ${notes}`));
+  }
 
+  if (isCompleted) {
+    li.setAttribute("class", "completed-task");
+    li.appendChild(document.createElement("br"));
+  } else {
     li.appendChild(document.createElement("br"));
     const completeBtn = document.createElement("button");
     completeBtn.setAttribute("id", "feature-button");
     completeBtn.innerHTML = "Complete";
     completeBtn.addEventListener("click", (e) => {
-      li.setAttribute("class", "completed-task");
       completeBtn.remove();
+      // updating taskList
+      const completedTask = taskList.find((task) => task.title === description);
+      completedTask.isCompleted = true;
+      localStorage.setItem("tasks", JSON.stringify(taskList));
+      location.reload();
     });
     li.appendChild(completeBtn);
-
-    const deleteBtn = document.createElement("button");
-    deleteBtn.setAttribute("id", "feature-button");
-    deleteBtn.innerHTML = "Delete";
-    deleteBtn.addEventListener("click", (e) => {
-      li.remove();
-    });
-    li.appendChild(deleteBtn);
-
-    taskList.appendChild(li);
-
-    titleInput.value = "";
-    dateInput.value = "";
-    notesInput.value = "";
   }
-}
+  const deleteBtn = document.createElement("button");
+  deleteBtn.setAttribute("id", "feature-button");
+  deleteBtn.innerHTML = "Delete";
+  deleteBtn.addEventListener("click", (e) => {
+    li.remove();
+    // updating taskList
+    taskList = taskList.filter((task) => task.title !== description);
+    localStorage.setItem("tasks", JSON.stringify(taskList));
+  });
+  li.appendChild(deleteBtn);
+
+  return li;
+};
+
+const ulOutput = () => {
+  taskList.forEach((task) => {
+    ulTasks.append(
+      liOutput(task.title, task.deadline, task.notes, task.isCompleted)
+    );
+  });
+};
+
+const sortAscDeadline = () => {
+  taskList.sort((a, b) => a.deadline > b.deadline);
+  localStorage.setItem("tasks", JSON.stringify(taskList));
+  location.reload();
+};
+
+const sortDesDeadline = () => {
+  taskList.sort((a, b) => a.deadline < b.deadline);
+  localStorage.setItem("tasks", JSON.stringify(taskList));
+  location.reload();
+};
+
+const sortComplete = () => {
+  taskList.sort((a, b) => a.isCompleted > b.isCompleted);
+  localStorage.setItem("tasks", JSON.stringify(taskList));
+  location.reload();
+};
+
+const sortIncomplete = () => {
+  taskList.sort((a, b) => a.isCompleted < b.isCompleted);
+  localStorage.setItem("tasks", JSON.stringify(taskList));
+  location.reload();
+};
+
+ulOutput();
